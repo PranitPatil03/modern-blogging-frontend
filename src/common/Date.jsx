2f2b0c21466dbdb65c44ec5888f32892a3f09445
Pranit <patilpranit3112@gmail.com>
2023-12-01 14:43:08 +0530

    Home Page completed till Trending Pages

--- src/common/Date.jsx
+++ src/common/Date.jsx
@@ -1,5 +1,32 @@
-const Date = () => {
-  return <div>Date</div>;
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const getDay = (timestamp) => {
+  const date = new Date(timestamp);
+
+  return `${date.getDate()} ${months[date.getMonth()]}`;
 };
 
-export default Date;
+export default getDay;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,89 @@
+import axios from "axios";
 import PageAnimation from "../common/PageAnimation";
 import InPageNavigation from "../components/InpageNavigation";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import BlogPostCard from "../components/BlogPostCard";
+import MinimalBlogPost from "../components/NobannerBlogPost";
 
 const HomePage = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [trendingBlogs, setTrendingBlogs] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const fetchTrendingBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/trending-blogs")
+      .then(({ data }) => {
+        setTrendingBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+    fetchTrendingBlogs();
+  }, []);
+
   return (
     <PageAnimation>
       <section className="h-cover flex justify-center gap-10">
-      
         <div className="w-full">
-          <InPageNavigation defaultHidden={["treading blogs"]} routes={["home", "treading blogs"]}>
-            
-            <h1> This is home page </h1>
-            <h1> This is treading page </h1>
-            
+          <InPageNavigation
+            defaultHidden={["treading blogs"]}
+            routes={["home", "treading blogs"]}
+          >
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <PageAnimation
+                      key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </PageAnimation>
+                  );
+                })
+              )}
+            </>
+            {trendingBlogs == null ? (
+              <Loader />
+            ) : (
+              blogs.map((blog, i) => {
+                return (
+                  <PageAnimation
+                    key={i}
+                    transition={{ duration: 1, delay: i * 0.1 }}
+                  >
+                    <MinimalBlogPost blog={blog} index={i}/>
+                  </PageAnimation>
+                );
+              })
+            )}
           </InPageNavigation>
         </div>
 
-        <div>
-
-        </div>
-        
+        <div></div>
       </section>
     </PageAnimation>
-  )
+  );
 };
 
 export default HomePage;
