d8676a90f81683d1ccf8610341a41b69c6b0380f
Pranit <patilpranit3112@gmail.com>
2023-12-08 14:46:32 +0530

    Comment Api integreted

--- src/components/CommentField.jsx
+++ src/components/CommentField.jsx
@@ -1,8 +1,50 @@
 import { comment } from "postcss";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { UserContext } from "../App";
+import axios from "axios";
+import { BlogContext } from "../pages/BlogPage";
 
 const CommandField = ({ action }) => {
   const [comment, setComment] = useState();
+
+  const {
+    userAuth: { accessToken },
+  } = useContext(UserContext);
+
+  const {
+    blog: {
+      _id,
+      author: { _id: blog_author },
+    },
+  } = useContext(BlogContext);
+
+  const handleComment = () => {
+    if (!accessToken) {
+      return toast.error("Login first to add comment");
+    }
+
+    if (!comment.length) {
+      return toast.error("Write something to add the comment");
+    }
+
+    axios
+      .post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/add-comment",
+        { _id, blog_author, comment },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log("data blog", data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
@@ -12,7 +54,9 @@ const CommandField = ({ action }) => {
         placeholder="Leave a comment..."
         className="input-box pl-5 placeholder:text-dark-grey resize-none h-[150px] overflow-auto"
       ></textarea>
-      <button className="btn-dark mt-5 px-10">{action}</button>
+      <button className="btn-dark mt-5 px-10" onClick={handleComment}>
+        {action}
+      </button>
     </>
   );
 };
